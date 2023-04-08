35e72177f58ccc3c78a66f14300348b0aac4590e
tykervella <kervellatyler@gmail.com>
2023-04-08 16:35:14 -0700

    added axios dependency, added .env variables, got programs in /auth route to allow user to connect to spotify which gives them a specific access_token that's stored in session storage

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-const userAuth = `https://accounts.spotify.com/authorize?client_id=44dd607d18f74af59288c28ecbb77a63&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Faccount%2F&scope=user-top-read
+const userAuth = `https://accounts.spotify.com/authorize?client_id=44dd607d18f74af59288c28ecbb77a63&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2profile%2F&scope=user-top-read
 `
