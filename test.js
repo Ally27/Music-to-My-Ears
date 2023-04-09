525781394acde32bf84e3f230c6d05b47b77eb1b
marleyschneiderr <marley33@uw.edu>
2023-04-09 12:00:10 -0700

    Adding updates to several handlebars pages

--- test.js
+++ test.js
@@ -1,2 +1,42 @@
-const userAuth = `https://accounts.spotify.com/authorize?client_id=44dd607d18f74af59288c28ecbb77a63&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2profile%2F&scope=user-top-read
-`
+const axios = require("axios");
+
+async function getTopTracks() {
+  try {
+    const rawData = await axios.get(
+      "https://api.spotify.com/v1/playlists/03HusFeEsbdRBAVU88VF26",
+      {
+        headers: {
+          Authorization: "Bearer BQCYzInUXj2vM9qsIsdFvKN1TOeNKlK5GN51B5cpcagdafBLZP9lrqH01Ivex0KIR0dTcdTu3-ZmbkDdlig1dheI4GA-f-HYC4Dj2mR4ZnLjcT1C3LRpBDL-Cqs5q1iGIWuIvdd-WMtjn8W7BqKJqWM2dVG3OnKktVzUIoX-NJLcBgSRCf4V2h4gjp8"
+        },
+      }
+    )
+
+    const rawTData = await axios.get(
+        "https://api.spotify.com/v1/playlists/03HusFeEsbdRBAVU88VF26/tracks?limit=10&locale=en-US,en;q=0.5",
+        {
+          headers: {
+            Authorization: "Bearer BQCYzInUXj2vM9qsIsdFvKN1TOeNKlK5GN51B5cpcagdafBLZP9lrqH01Ivex0KIR0dTcdTu3-ZmbkDdlig1dheI4GA-f-HYC4Dj2mR4ZnLjcT1C3LRpBDL-Cqs5q1iGIWuIvdd-WMtjn8W7BqKJqWM2dVG3OnKktVzUIoX-NJLcBgSRCf4V2h4gjp8"
+          },
+        }
+      )
+    
+    // Handle the retrieved data here
+    const data = rawData.data;
+    const trackData = rawTData.data.items;
+    const imageArr = data.images;
+    const postLink = data.href;
+    const playlistTitle = data.name;
+    
+    console.log(imageArr, postLink, playlistTitle,trackData)
+
+  } catch (error) {
+    console.error(error);
+    // Handle the error here
+  }
+}
+
+// Call the function to execute the code
+getTopTracks();
+
+
+
