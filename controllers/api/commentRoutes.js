fdea465b388f07325f8edf411b6f4d52f0522268
tykervella <kervellatyler@gmail.com>
2023-04-16 18:08:10 -0700

    finishing tweaks

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -11,4 +11,13 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
