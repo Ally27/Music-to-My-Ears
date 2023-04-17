f98f7b4e137950d1fc895f83488556e347fac2de
tykervella <kervellatyler@gmail.com>
2023-04-16 17:34:06 -0700

    added 404 route/ update password route

--- controllers/index.js
+++ controllers/index.js
@@ -14,5 +14,9 @@ router.use('/posts', postRoutes);
 router.use('/search', searchRoutes);
 router.use('/community', communityRoutes)
 
+router.get('*', function(req, res){
+    res.status(404).render('404', {logged_in: req.session.logged_in});
+  });
+
 
 module.exports = router;
